3cc8ff2748280eabe3ca76a7db720b1ef8cbccb8
Grace Palacz <gracelizbuckley@gmail.com>
2022-03-22 13:42:10 +0000

    amended view helper to get rid of errors and added divs to make image work

--- src/Classes/ViewHelpers/DinoViewHelper.php
+++ src/Classes/ViewHelpers/DinoViewHelper.php
@@ -7,8 +7,10 @@ use DinoFinder\Classes\Entities\DinoEntity;
 class DinoViewHelper {
     public static function displaySingleDino(DinoEntity $dinos) {
         $result = '<h2>' . $dinos->getSpecies() . '</h2>';
-        $result .= '<img src= "' . $dinos->getImgUrl() . '"alt="An image of a' . $dinos->getSpecies() . '"/>';
-        $result .= '<h4>' . $dinos->getFoodTypes() . '</h4>';
+        $result .= '<div class="imageContainer">';
+        $result .= '<img src= "' . $dinos->getImageUrl() . '"alt="An image of a' . $dinos->getSpecies() . '"/>';
+        $result .= '</div>';
+        $result .= '<h4>' . $dinos->getDiet() . '</h4>';
         return $result;
     }
     public static function displayMultipleDinos(array $dinos) {
