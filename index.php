dd3347646d5b1b7a6228dba2ed11b7d39a84d2a4
Grace Palacz <gracelizbuckley@gmail.com>
2022-03-22 15:28:39 +0000

    added normalize and images

--- index.php
+++ index.php
@@ -8,7 +8,16 @@ $displayDinos = \DinoFinder\Classes\ViewHelpers\DinoViewHelper::displayMultipleD
 
 ?>
 
+
+<html lang="en">
+<head>
+    <title>Title</title>
+    <link rel="shortcut icon" type="image/jpg" href="favicon.png"/>
+    <link href= "normalize.css" type="text/css" rel="stylesheet"/>
+    <meta name="viewport" content="width=device-width">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
 <body>
 <?php echo $displayDinos; ?>
 </body>
-
+</html>
