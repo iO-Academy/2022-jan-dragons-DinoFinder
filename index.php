83b31eeddcfc620e33a4b208b576b040db84d1ba
Gabriel Rowan <gabrielrowan82@gmail.com>
2022-03-22 12:04:30 +0000

    took out var_dump as was only needed for testing purposes

--- index.php
+++ index.php
@@ -5,4 +5,3 @@ require_once 'vendor/autoload.php';
 $db = new \PDO('mysql:host=db; dbname=dinosaurs', 'root', 'password');
 $dinos = \DinoFinder\Classes\Hydrators\DinoHydrator::getAllDinos($db);
 
-var_dump($dinos);
