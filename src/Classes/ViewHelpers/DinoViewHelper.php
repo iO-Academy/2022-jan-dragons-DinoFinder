36bd546b5b3656029c217eff057165ee13719e80
Grace Palacz <gracelizbuckley@gmail.com>
2022-03-22 14:14:13 +0000

    added doc blocks and type hints as per code review

--- src/Classes/ViewHelpers/DinoViewHelper.php
+++ src/Classes/ViewHelpers/DinoViewHelper.php
@@ -5,15 +5,29 @@ namespace DinoFinder\Classes\ViewHelpers;
 use DinoFinder\Classes\Entities\DinoEntity;
 
 class DinoViewHelper {
-    public static function displaySingleDino(DinoEntity $dinos) {
-        $result = '<h2>' . $dinos->getSpecies() . '</h2>';
-        $result .= '<div class="imageContainer">';
-        $result .= '<img src= "' . $dinos->getImageUrl() . '"alt="An image of a' . $dinos->getSpecies() . '"/>';
+    /**
+     * displays a single dinosaur's image, name and diet
+     *
+     * @param DinoEntity $dinos
+     * @return string - concatenated string of dinosaur image, name and diet
+     */
+    public static function displaySingleDino(DinoEntity $dinos) : string {
+
+        $result = '<div class="imageContainer">';
+        $result .= '<img src= "' . $dinos->getImageUrl() . '" alt="An image of a' . $dinos->getSpecies() . '"/>';
         $result .= '</div>';
+        $result .= '<h2>' . $dinos->getSpecies() . '</h2>';
         $result .= '<h4>' . $dinos->getDiet() . '</h4>';
         return $result;
     }
-    public static function displayMultipleDinos(array $dinos) {
+
+    /**
+     * displays multiple dinosaur images, names and diets
+     *
+     * @param array $dinos
+     * @return string - concatenated strings of multiple dinosaur images, names and diets
+     */
+    public static function displayMultipleDinos(array $dinos) : string {
         $result = '';
         foreach ($dinos as $dino) {
             $result .= self::displaySingleDino($dino);
