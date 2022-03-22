56bb1b93c4818b2ba9246084c792b595357f2845
Ethan Garrett <garrettethan63@gmail.com>
2022-03-22 11:20:27 +0000

    Added DinoEntity and DinoHydrator

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
-require_once('vendor/autoload.php');
+<?php
 
+require_once 'vendor/autoload.php';
+
+$db = new PDO('mysql:host=db; dbname=dinosaurs', 'root', 'password');
+$dinos = \DinoFinder\classes\Hydrators\DinoHydrator\DinoHydrator::getAllDinos($db);
+
+var_dump($dinos);
